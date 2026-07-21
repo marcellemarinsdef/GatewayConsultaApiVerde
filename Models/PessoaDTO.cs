10de49d6e2365fc8f1303e083f470868a47237bb
Marcelle Marins <marcelle.marins@defensoria.rj.def.br>
2026-07-21 15:58:35 -0300

    :sparkles: feat: adição do endpoint de consulta de pessoa por cpf

---
+++ Models/PessoaDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GatewayConsultaApiVerde.Models
+{
+    public class PessoaDTO
+    {
+        public class RespostaDTO
+        {
+            [JsonPropertyName("dados")]
+            public Pessoa Dados { get; set; }
+        }
+        public class Pessoa
+        {
+            [JsonPropertyName("idPessoa")]
+            public int Id { get; set; }
+        }
+    }
+}
---
+++ Services/PessoaService.cs
@@ -0,0 +1,42 @@
+﻿using GatewayConsultaApiVerde.Exceptions;
+using GatewayConsultaApiVerde.Models;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace GatewayConsultaApiVerde.Services
+{
+    public class VerdeApiClient : IVerdeApiClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ConsultaVerdeSettings _consultaVerdeSettings;
+
+        public VerdeApiClient(HttpClient httpClient, IOptions<ConsultaVerdeSettings> consultaVerdeSettings)
+        {
+            _httpClient = httpClient;
+            _consultaVerdeSettings = consultaVerdeSettings.Value;
+        }
+
+        public async Task<PessoaDTO.RespostaDTO> GetPessoaAsync(string cpfPessoa)
+        {
+            var clientId = _consultaVerdeSettings.ClientID;
+            var token = _consultaVerdeSettings.Token;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"pessoa?cpf={cpfPessoa}");
+            request.Headers.Add("Authorization", token);
+            request.Headers.Add("X-Client-ID", clientId);
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApiException((int)response.StatusCode);
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<PessoaDTO.RespostaDTO>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,               
+            });
+        }
+    }
+}
