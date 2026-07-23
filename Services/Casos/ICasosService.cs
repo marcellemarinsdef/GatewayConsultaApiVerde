4593ef843e72733f57e811019389a8c7f4cfdfc8
Marcelle Marins <marcelle.marins@defensoria.rj.def.br>
2026-07-23 14:52:41 -0300

    :sparkles: feat: adição do endpoint de casos, listando apenas os com status em aberto, criação do SharedService para evitar repetição de codigo

---
+++ Services/Casos/ICasosService.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using GatewayConsultaApiVerde.Models;
+
+namespace GatewayConsultaApiVerde.Services.Casos
+{
+    public interface ICasosService
+    {
+        Task<CasosDTO.RespostaDto> GetCasosAsync(string cpfPessoa);
+    }
+}
